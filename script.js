e49313890b47d999b54d706a0193bf9a9c987acd
janamanzana <96806525+janamanzana@users.noreply.github.com>
2022-06-15 21:37:10 +0200

    Added score change

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 // Create random number betwen 1 and 20
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-document.querySelector('.number').textContent = secretNumber;
+// Change score
+let score = 20;
 
 // Select guess value
 
@@ -17,12 +18,28 @@ function checkNumber() {
     document.querySelector('.message').textContent = 'No number! 🚫';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number! 🎉';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Number is too low!';
+    if (score >= 1) {
+      document.querySelector('.message').textContent = 'Number is too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 💔';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Number is too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Number is too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 💔';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 }
-
 //Select button and add event listener
 document.querySelector('.check').addEventListener('click', checkNumber);
