be2ae86c5ce749af24fa559407c38389130cb93f
janamanzana <96806525+janamanzana@users.noreply.github.com>
2022-06-15 20:55:20 +0200

    Added check number function and event listened to the check button

--- script.js
+++ script.js
@@ -4,3 +4,25 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.number').textContent = secretNumber;
+
+// Select guess value
+
+console.log(secretNumber);
+
+// Check number function
+function checkNumber() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number! 🚫';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number! 🎉';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Number is too low!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Number is too high!';
+  }
+}
+
+//Select button and add event listener
+document.querySelector('.check').addEventListener('click', checkNumber);
